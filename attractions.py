e09c83713d744c0030ead94da06a165aaa4165da
Gustavo <gugafrs1713@gmail.com>
2023-02-09 21:40:59 -0300

    maybe it'll work this time

---
+++ attractions.py
@@ -0,0 +1,30 @@
+attractions = {
+    1: 'Beco do Batman', 
+    2: 'MASP (Museu de Arte de São Paulo)', 
+    3: 'Parque Ibirapuera', 
+    4: 'Pinacoteca',
+    5: 'Mercado Municipal', 
+    6: 'Theatro Municipal', 
+    7: 'Museu do Futebol', 
+    8: 'Catedral Metropolitana',
+    9: 'Liberdade', 
+    10: 'Jardim Botânico', 
+    11: 'Avenida Paulista', 
+    12: 'Casa das Rosas'
+}
+
+attractions_stations = {
+    'Beco do Batman': '',
+    'MASP (Museu de Arte de São Paulo)': '', 
+    'Parque Ibirapuera': '', 
+    'Pinacoteca': '',
+    'Mercado Municipal': '', 
+    'Theatro Municipal': '', 
+    'Museu do Futebol': '', 
+    'Catedral Metropolitana': '',
+    'Liberdade': 'Japão-Liberdade', 
+    'Jardim Botânico': '', 
+    'Avenida Paulista': '', 
+    'Casa das Rosas': ''
+}
+print(len(attractions))
